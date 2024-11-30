47cbbeeb624b727d48a400d744c416082726e9a4
Arham-jawad896 <jawadarham896@gmail.com>
2024-11-30 10:06:15 +0500

    Added git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/ui/navbar/Navbar";
-import Home from "./pages/Home"; // Example of a page component
-
 function App() {
   return (
     <Router>
@@ -12,7 +10,6 @@ function App() {
       {/* Define routes for different pages */}
       <Routes>
         {/* Specify the path and the component to render */}
-        <Route path="/" element={<Home />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>

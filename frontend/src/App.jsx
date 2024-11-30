ffb0ddbb40e72ab344ddfbccde951f28f86b0e69
Arham-jawad896 <jawadarham896@gmail.com>
2024-11-30 12:02:15 +0500

    Removed unnacessary things

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/ui/navbar/Navbar";
+
 function App() {
   return (
     <Router>
-      {/* Navbar will be present across all pages */}
-      <Navbar />
-
-      {/* Define routes for different pages */}
       <Routes>
         {/* Specify the path and the component to render */}
         {/* Add more routes as needed */}
